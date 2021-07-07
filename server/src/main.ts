3e5d04469eb7a53d4711fe3b2316cbae7ec8f44a
Sergei-Brykov <sergeibrykovdew@gmail.com>
2021-07-07 20:32:01 +0300

    fix main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,14 +1,14 @@
-import {NestFactory} from "@nestjs/core"
-import {AppModule} from "./app.module";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 const start = async () => {
-    try {
-        const PORT = 3000
-        const app = await NestFactory.create(AppModule)
-        await app.listen(PORT, () => console.log(`Server up in port = ${PORT}`))
-    } catch (e) {
+  try {
+    const PORT = 3000;
+    const app = await NestFactory.create(AppModule);
+    await app.listen(PORT, () => console.log(`Server up in port = ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+};
 
-    }
-}
-
-start()
+start();
